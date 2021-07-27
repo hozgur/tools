6be698f5d45f819ebfc2164a86eaaefe2a39918d
Hakan ÖZGÜR <hakan.ozgur@gmail.com>
2021-07-27 10:39:12 +0000

    node install improvement

--- install/install-node16.sh
+++ install/install-node16.sh
@@ -1,3 +1,7 @@
 # Using Ubuntu
+
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
+
+#Üstteki yöntem işe yaramaz ise
+#install nvm then nvm install 16.5.0
