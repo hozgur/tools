29daabbc803525fdce378e8b92e8a1aff57d5e4f
Hakan ÖZGÜR <hakan@ait.com.tr>
2021-08-01 22:33:22 +0300

    http-request template eklendi.

--- templates/node/http-request/index.js
+++ templates/node/http-request/index.js
@@ -1,16 +1,33 @@
 const { default: axios } = require("axios")
 
 
-// express template projesine istek gönderir.
+// express template projesine istek gönderir. (get)
 function sendGetRequestSync() {
     const url = "http://localhost:5000/api"
-    console.log('Sending Request.');
-    axios.get(url).then((response) => {
-        console.log('data : ',response.data)
-    }).catch((err) => {
+    console.log('Sending Get Request.');
+    axios.get(url)
+    .then((response) => {
+        console.log('Get Response Data : ',response.data)
+    })
+    .catch((err) => {
+        console.log("error: ",err.message)
+    })
+}
+
+function sendPostRequestSync() {
+    const url = "http://localhost:5000/api"
+    console.log('Sending Post Request.');
+    axios.post(url, {
+        param1: "Param1-text",
+        param2:"param2-text"
+    })
+    .then((response) => {
+        console.log('Post-Request Response Data : ',response.data)
+    })
+    .catch((err) => {
         console.log("error: ",err.message)
     })
 }
 
 sendGetRequestSync()
-
+sendPostRequestSync()
