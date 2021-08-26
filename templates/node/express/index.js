5ec5340c8abd30ee907b84657ff0d96c840eb224
Hakan ÖZGÜR <hakan.ozgur@gmail.com>
2021-08-26 08:58:47 +0000

    node express projesine ek özellikler ve açıklamalar eklendi.

--- templates/node/express/index.js
+++ templates/node/express/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 
+// url encoded olarak gelen veriyi almak için
 app.use(express.urlencoded({ extended: true }));
+// json olarak gelen body data'sını almak için
 app.use(express.json());
 
+// Statik Dosyaları paylaşmak için 
+app.use(express.static('public'))
+// Birden fazla tanımlanabilir.
+app.use(express.static('files'))
+
+// Farklı istekleri ayrı dosyalarda yönetmek için rooter kullanımı.
 app.use('/api',require("./routes/api-sample"));
+
+// Çağrımlara verilen örnekler api-sample.js dosyasında.
 
 const PORT = process.env.PORT || 5000;
 
--- templates/node/express/routes/api-sample.js
+++ templates/node/express/routes/api-sample.js
@@ -7,12 +7,15 @@ const data = require('../sample-data.json')
 const errorMessages = {
   ERR_NO_DATA: 'veri gönderilmesi gerekli.'
 }
-
+// /api olarak yapılan çağrımlarda kullanılan get ve post metodları
 router.get("/", (req,res) => {
+
+    // data verisini json olarak göndermek için.
     res.json(data);
 });
 
-
+// post 
+// curl -X POST -H "Content-Type: application/json" -d '{"username":"xyz","password":"xyz"}' http://localhost:5000/api
 router.post("/", (req,res) => {
   
   // post kullanıyorsak 
@@ -26,6 +29,8 @@ router.post("/", (req,res) => {
   }
 });
 
+// api/param1/param2 şeklindeki çağrıma cevap vermek için 
+// curl -X POST -H "Content-Type: application/json" -d '{"username":"xyz","password":"xyz"}' http://localhost:5000/api/param1/param2
 router.post("/:param1/:param2", (req,res) => {
   
   console.log("Param 1 :",req.params.param1);
@@ -33,12 +38,27 @@ router.post("/:param1/:param2", (req,res) => {
   // post kullanıyorsak 
   if(req.body)
   {
-    res.json(req.body);
+    res.json({
+      "param1": req.params.param1,
+      "param2": req.params.param2,
+      "body": req.body
+    });
   }
   else
   {
     res.status(400).send(errorMessages.ERR_NO_DATA)
   }
+});
+
+// urlencoded ile veri alma örneği:
+// Örnek Çağrım: http://localhost:5000/api/querytest?param1=value1&param2=value2
+router.get("/querytest", (req,res) => {
+  
+  console.log("Param 1 :",req.query.param1);
+  console.log("Param 2 :",req.query.param2);
+  
+  res.json({"param1": req.query.param1, "param2": req.query.param2});
+  
 });
 
 module.exports = router;
